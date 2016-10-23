ebe1fbbfb22d506792ce1474b80c7a891bdbc60e
smaharj1 <smaharj1@ramapo.edu>
2016-10-23 09:30:50 -0400

    aaa

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
     $(".nora-intro").hide();
     $("#nora-nora").hide();
+    
 
     $(".nora-intro").fadeIn(4000, function() {
         // Animation complete
@@ -20,14 +21,27 @@ $(document).ready(function () {
         updatePictures(parsedData);
     });
 
-    $.ajax({
-        method: "POST",
-        url: "http://cbf1bcba.ngrok.io/bankAnalytics",
-        datatype: "json",
-        data: { phone: "+19084774708", url : "https://api.twilio.com/2010-04-01/Accounts/AC6cde470a46432c2d0860e433c15c3e7c/Messages/MM7cc9790da51354363758fc06ebd46dd4/Media/ME20aec842371faa59f3cffec10a28b37c"} ,
-    }).done(function(data) {
-        alert(data+"");
+
+    $('body').on('click','td img', function () {
+        var clickedUrl = $(this).attr("src");
+
+        $.ajax({
+            method: "POST",
+            url: "http://cbf1bcba.ngrok.io/bankAnalytics",
+            datatype: "json",
+            data: { phone: "+19084774708", url : clickedUrl} ,
+        }).done(function(data) {
+            var pData = JSON.parse(data);
+            
+            var content = $('.modal-content');
+            content.append("<img src="+clickedUrl+"/>");
+            
+
+            
+        });
     });
+
+    
 
 
 });
@@ -43,6 +57,6 @@ function updatePictures(parsedData) {
             $('.images').append("<tr></tr>");
         }
 
-        $('.images').children().last().append("<td><img src="+imageURL+"></td>");
+        $('.images').children().last().append("<td><img class=\"img-responsive img-thumbnail\" data-toggle=\"modal\" data-target=\".bs-example-modal-lg\"src="+imageURL+"></td>");
     }
 }
